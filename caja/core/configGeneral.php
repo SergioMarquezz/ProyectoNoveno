107d1481d4ef2077bf03992fa7c6688381b5311a
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2019-06-25 09:17:58 -0500

    Se elimino carpeta

--- caja/core/configGeneral.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    const COMPANY = "Sistema de Automatización Estudiantil";
-    const ACRONYM = "SAE";
-    const SERVER = "http://localhost:8080/caja/";
-    
-?>
--- caja/views/modules/fecha.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$date = "Fecha de hoy <br> " . date("d") . " / " . date("m") . " / " . date("Y");
-$year = date("Y");
-
-?>
